5682ea747427f5fbabac4fd9bfac6cd8578a702b
Papdji <mbah0397@gmail.com>
2021-12-21 18:19:32 +0000

    version 2

--- backend/src/main/java/com/formgroupe/backend/services/GroupeServiceImp.java
+++ backend/src/main/java/com/formgroupe/backend/services/GroupeServiceImp.java
@@ -1,7 +1,18 @@
 package com.formgroupe.backend.services;
 
+import com.formgroupe.backend.model.Groupe;
+import com.formgroupe.backend.repositories.GroupeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GroupeServiceImp implements GroupeService{
+
+    @Autowired
+    GroupeRepository groupeRepository;
+
+    @Override
+    public Groupe ajoutGroupe(Groupe groupe) {
+        return groupeRepository.save(groupe);
+    }
 }
