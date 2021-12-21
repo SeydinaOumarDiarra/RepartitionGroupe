e451340a286365c46745183cde205a75f40e5679
daouSam <sambadaou61@gmail.com>
2021-12-21 18:20:20 +0000

    c'est tout pour aujourd'hui

--- backend/src/main/java/com/formgroupe/backend/controller/TacheController.java
+++ backend/src/main/java/com/formgroupe/backend/controller/TacheController.java
@@ -1,11 +1,34 @@
 package com.formgroupe.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.formgroupe.backend.model.Tache;
+import com.formgroupe.backend.services.TacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/repartition")
 @CrossOrigin("*")
 public class TacheController {
+
+    @Autowired
+    TacheService tacheService;
+
+    @PostMapping("/ajoutTache")
+    public Tache ajoutTAche(Tache tache){
+        return tacheService.ajoutTache(tache);
+    }
+
+    @GetMapping("/listeTache")
+    public List<Tache> listeTache(){
+        return tacheService.listeTache();
+    }
+
+    @PutMapping("/modifierTache/{id}")
+    public void modifierTache(@PathVariable Long id, @RequestBody Tache tache){
+        tacheService.modifierTache(id, tache);
+    }
+
+
 }
