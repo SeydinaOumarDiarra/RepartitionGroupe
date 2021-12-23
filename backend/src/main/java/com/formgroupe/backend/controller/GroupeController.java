7ace344d6e41054ca1e1ed676e9f5bae4685d715
Papdji <mbah0397@gmail.com>
2021-12-23 09:22:22 +0000

    modifier

--- backend/src/main/java/com/formgroupe/backend/controller/GroupeController.java
+++ backend/src/main/java/com/formgroupe/backend/controller/GroupeController.java
@@ -2,8 +2,11 @@ package com.formgroupe.backend.controller;
 
 import com.formgroupe.backend.model.Groupe;
 import com.formgroupe.backend.services.GroupeService;
+import com.formgroupe.backend.services.ListeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/repartition")
@@ -16,4 +19,10 @@ public class GroupeController {
     public Groupe ajoutgroupe(@RequestBody Groupe groupe){
         return groupeService.ajoutGroupe(groupe);
     }
+    @GetMapping("/listGroupe")
+    public List<Groupe> ListGroupe(){
+        return groupeService.listgroupe();
+    }
+    @DeleteMapping("/suppGroupe/{id}")
+    public void suppGroupe(@PathVariable Long id) {groupeService.suppGroup(id);}
 }
--- backend/src/main/java/com/formgroupe/backend/services/GroupeService.java
+++ backend/src/main/java/com/formgroupe/backend/services/GroupeService.java
@@ -3,8 +3,12 @@ package com.formgroupe.backend.services;
 import com.formgroupe.backend.model.Groupe;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface GroupeService {
 
     public Groupe ajoutGroupe(Groupe groupe);
+    public List<Groupe> listgroupe();
+    public void suppGroup(Long id);
 }
